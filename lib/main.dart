b5fdc2fba4062093db7761225510a1aebe12bef5
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2022-11-24 12:04:40 +0800

    email verification and link views

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_notes_app/firebase_options.dart';
 import 'package:firebase_notes_app/views/login_view.dart';
+import 'package:firebase_notes_app/views/register_view.dart';
+import 'package:firebase_notes_app/views/verify_email_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +14,42 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
+      routes: {
+        "/login/": (context) => const LoginView(),
+        "/register/": (context) => const RegisterView(),
+      },
     ),
   );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
 }
