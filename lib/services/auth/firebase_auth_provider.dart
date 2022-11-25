6acf16aff5f4e24f9dd7a6ba8b8aaf7b6fed464d
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2022-11-25 17:01:35 +0800

    auth service

---
+++ lib/services/auth/firebase_auth_provider.dart
@@ -0,0 +1,98 @@
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
+import 'package:firebase_notes_app/services/auth/auth_exceptions.dart';
+import 'package:firebase_notes_app/services/auth/auth_provider.dart';
+import 'package:firebase_notes_app/services/auth/auth_user.dart';
+
+class FirebaseAuthProvider implements AuthProvider {
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final user = currentUser;
+      if (user != null) {
+        return user;
+      }
+      throw UserNotLoggedInAuthException();
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "weak-password":
+          throw WeakPasswordAuthException();
+        case "email-already-in-use":
+          throw EmailAlreadyInUseAuthException();
+        case "invalid-email":
+          throw InvalidEmailAuthException();
+        default:
+          throw GenericAuthException();
+      }
+    } catch (_) {
+      throw GenericAuthException();
+    }
+  }
+
+  @override
+  AuthUser? get currentUser {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return AuthUser.fromFirebase(user);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final user = currentUser;
+      if (user != null) {
+        return user;
+      } else {
+        throw UserNotLoggedInAuthException();
+      }
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "user-not-found":
+          throw UserNotFoundAuthException();
+        case "wrong-password":
+          throw WrongPasswordAuthException();
+        default:
+          throw GenericAuthException();
+      }
+    } catch (_) {
+      throw GenericAuthException();
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      await FirebaseAuth.instance.signOut();
+    } else {
+      throw UserNotLoggedInAuthException();
+    }
+  }
+
+  @override
+  Future<void> sendEmailVerification() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      await user.sendEmailVerification();
+    } else {
+      throw UserNotLoggedInAuthException();
+    }
+  }
+}
