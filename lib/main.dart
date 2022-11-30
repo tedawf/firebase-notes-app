b922e6b20fdd122dc2256161d7d4f117838445e0
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2022-11-30 23:56:09 +0800

    edit existing note

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_notes_app/constants/routes.dart';
 import 'package:firebase_notes_app/services/auth/auth_service.dart';
 import 'package:firebase_notes_app/views/login_view.dart';
-import 'package:firebase_notes_app/views/notes/new_note_view.dart';
+import 'package:firebase_notes_app/views/notes/create_update_note_view.dart';
 import 'package:firebase_notes_app/views/notes/notes_view.dart';
 import 'package:firebase_notes_app/views/register_view.dart';
 import 'package:firebase_notes_app/views/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
